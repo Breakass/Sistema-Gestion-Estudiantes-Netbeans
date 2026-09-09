a567b4cdaebec271557aa8d1ad396ad1ac77c528
Luis Zafra <martinzafrag4@gmail.com>
2026-09-08 22:34:11 -0500

    feat: completar clase Estudiante

--- src/SistemaGestionEstudiantes/Estudiante.java
+++ src/SistemaGestionEstudiantes/Estudiante.java
@@ -4,10 +4,62 @@
  */
 package SistemaGestionEstudiantes;
 
-/**
- *
- * @author MZROCA - ELEKTAPC
- */
+import java.util.ArrayList;
+
 public class Estudiante {
-    
+
+    private String codigo;
+    private String nombre;
+    private ArrayList<Double> calificaciones;
+
+    public Estudiante(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.calificaciones = new ArrayList<>();
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Double> getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void agregarCalificacion(double nota) {
+        calificaciones.add(nota);
+    }
+
+    public double calcularPromedio() {
+        if (calificaciones.isEmpty()) {
+            return 0;
+        }
+
+        double suma = 0;
+
+        for (double nota : calificaciones) {
+            suma += nota;
+        }
+
+        return suma / calificaciones.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Codigo: " + codigo
+                + " | Nombre: " + nombre
+                + " | Promedio: " + calcularPromedio();
+    }
 }
