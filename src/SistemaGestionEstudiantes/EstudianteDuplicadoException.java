51a8a83c94540567359d4b5aa72d570998ebf558
Luis Zafra <martinzafrag4@gmail.com>
2026-09-08 22:47:48 -0500

    feat: agregar excepcion EstudianteDuplicadoException

---
+++ src/SistemaGestionEstudiantes/EstudianteDuplicadoException.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SistemaGestionEstudiantes;
+
+public class EstudianteDuplicadoException extends Exception {
+
+    public EstudianteDuplicadoException(String mensaje) {
+        super(mensaje);
+    }
+}
